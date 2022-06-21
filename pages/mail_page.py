2607649a66b9b8b4876ce9142343c5784f41efdc
efimovnik0707 <tomatik0707@gmail.com>
2022-06-21 12:34:53 +0200

    add comments for all functions

--- pages/mail_page.py
+++ pages/mail_page.py
@@ -4,14 +4,15 @@ from .locators import MailPageLocators
 import time
 
 
-
 class MailPage(BasePage):
 
+    # create a random string for subject and body
     def get_random_string(self, length):
         random_string = self.string.ascii_lowercase + self.string.digits
         return ''.join(self.random.choice(random_string) for i in range(length))
 
-    def try_click_element(self, selector, refresh = False):
+    # try to click on the INBOX several times to avoid the error
+    def try_click_element(self, selector, refresh=False):
         current_try = 0
         TRIES_MAXIMUM = 10
         while current_try < TRIES_MAXIMUM:
@@ -29,10 +30,37 @@ class MailPage(BasePage):
             current_try += 1
         raise Exception("Click did not work :(")
 
+    # function for click on the INBOX
     def load_inbox(self):
         print('load inbox')
         self.try_click_element(MailPageLocators.INBOX_BTN, True)
 
+    # count for all functions
+    def prepare_test(self):
+        self.count = 1
+
+    # send mail with random subject and random body
+    def send_mail(self):
+        time.sleep(3)
+        self.random_subject = get_random_string(10)
+
+        self.try_click_element(MailPageLocators.NEW_MAIL)
+        time.sleep(1)
+        self.input_email_receiver = self.browser.find_element(
+            *MailPageLocators.EMAIL_RECEIVER).send_keys("testhookah@aol.com")
+
+        subject = (self.random_subject)
+        self.browser.find_element(*MailPageLocators.SUBJECT).send_keys(subject)
+
+        random_body = get_random_string(10)
+        body = (random_body)
+        self.browser.find_element(*MailPageLocators.BODY).send_keys(body)
+        self.try_click_element(MailPageLocators.SEND_BTN)
+
+        print("Mail number:", self.count, "- sent")
+        time.sleep(3)
+
+    # trying to find a random_subject in 3 last rows
     def find_random_subject(self, random_subject):
         # print("find_random_subject --->", random_subject)
         timeStart = time.time()
@@ -49,47 +77,14 @@ class MailPage(BasePage):
                     # print("error... :(", e)
                     pass
             time.sleep(1)
-
-    def send_mail(self):
-        time.sleep(3)
-        self.random_subject = get_random_string(10)
-
-        self.try_click_element(MailPageLocators.NEW_MAIL)
-        time.sleep(1)
-        self.input_email_receiver = self.browser.find_element(*MailPageLocators.EMAIL_RECEIVER).send_keys("testhookah@aol.com")
-
-        subject = (self.random_subject)
-        self.browser.find_element(*MailPageLocators.SUBJECT).send_keys(subject)
-
-        random_body = get_random_string(10)
-        body = (random_body)
-        self.browser.find_element(*MailPageLocators.BODY).send_keys(body)
-        self.try_click_element(MailPageLocators.SEND_BTN)
-
-        print("Mail number:", self.count, "- sent")
-        time.sleep(3)
-
+    
+    # function for start find deliverd message
     def try_to_find_deliverd_message(self):
         self.load_inbox()
         self.find_random_subject(self.random_subject)
         print("Mail number: ", self.count, "- found")
 
-    def find_subject_inbox(self, subject):
-        rows = self.browser.find_elements(*MailPageLocators.ROW);
-        # print("rows", len(rows))
-        if len(rows) == 0:
-            raise Exception("Zero rows :(")
-
-        for row_element in rows:
-            subject_element = row_element.find_element(*MailPageLocators.ROW_SUBJECT)
-            span_elements = subject_element.find_elements(*MailPageLocators.ROW_SUBJECT_SPAN)
-            row_subject = span_elements[0].text
-            row_body = span_elements[1].text
-            # print("row ->", row_subject, ", row_body", row_body)
-            if subject == row_subject:
-                subject_element.click()
-                return [row_subject, row_body]
-
+    # read mail and add data to dictionary
     def read_mail(self):
         # read mail
         self.load_inbox()
@@ -106,10 +101,31 @@ class MailPage(BasePage):
         self.try_click_element(MailPageLocators.DELETE_BTN_IN_MAIL)
         print("Mail number:", self.count, "- read")
 
+    # Trying to find a subject in all rows
+    def find_subject_inbox(self, subject):
+        rows = self.browser.find_elements(*MailPageLocators.ROW)
+        # print("rows", len(rows))
+        if len(rows) == 0:
+            raise Exception("Zero rows :(")
+
+        for row_element in rows:
+            subject_element = row_element.find_element(
+                *MailPageLocators.ROW_SUBJECT)
+            span_elements = subject_element.find_elements(
+                *MailPageLocators.ROW_SUBJECT_SPAN)
+            row_subject = span_elements[0].text
+            row_body = span_elements[1].text
+            # print("row ->", row_subject, ", row_body", row_body)
+            if subject == row_subject:
+                subject_element.click()
+                return [row_subject, row_body]
+
+    # send second mail with collected data from first mail
     def send_new_mail(self):
         # send_new_mail
         self.try_click_element(MailPageLocators.NEW_MAIL)
-        self.browser.find_element(*MailPageLocators.EMAIL_RECEIVER).send_keys("testhookah@aol.com")
+        self.browser.find_element(
+            *MailPageLocators.EMAIL_RECEIVER).send_keys("testhookah@aol.com")
 
         for keys, values in self.dict.items():
             new_dict = {'Letters': 0, 'Numbers': 0}
@@ -121,12 +137,14 @@ class MailPage(BasePage):
                         new_dict['Numbers'] += 1
 
             self.new_random_subject = get_random_string(10)
-            self.browser.find_element(*MailPageLocators.SUBJECT).send_keys(self.new_random_subject)
+            self.browser.find_element(
+                *MailPageLocators.SUBJECT).send_keys(self.new_random_subject)
             self.browser.find_element(*MailPageLocators.BODY).send_keys(
                 f'Received mail on theme {keys} with message: {values}. It contains {new_dict["Letters"]} letters and {new_dict["Numbers"]} numbers')
             self.try_click_element(MailPageLocators.SEND_BTN)
             time.sleep(2)
-    
+
+    # re-check deliver mail 
     def re_check_deliver_mail(self):
         # re-check deliver mail
         self.load_inbox()
@@ -134,6 +152,7 @@ class MailPage(BasePage):
         print(self.count, "new mail(s) sent")
         print('Task number:', self.count, '- complete')
 
+    # select and delete all messages except the last one
     def delete_all_messages(self):
         self.load_inbox()
         time.sleep(1)
@@ -141,13 +160,7 @@ class MailPage(BasePage):
         self.try_click_element(MailPageLocators.SELECT_FIRST_MSG)
         self.try_click_element(MailPageLocators.DELETE_BTN)
 
-    def prepare_test(self):
-        self.count = 1
-
+    # finall function for count
     def finalize_test_round(self):
         print(self.count, " round is finished")
         self.count += 1
-
-
-
-    
